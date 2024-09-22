8e9a1e362a7b8cff9340ac636c410804af40d6bf
schnatr <schnatr@gmail.com>
2024-09-22 14:15:37 +0700

    init serverside 3

---
+++ server/app/adapters/notificationAdapter.py
@@ -0,0 +1,3 @@
+class NotificationAdapter:
+    def adapt_notification_data(self, data):
+        return {"formatted_data": f"Adapted Notification: {data}"}
---
+++ server/app/externalService/notification/notificationService.py
@@ -0,0 +1,3 @@
+class NotificationService:
+    def process(self, data):
+        return {"message": f"Notification: {data}"}
